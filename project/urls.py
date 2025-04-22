0a3b8d842732c72f1447fef79c6d72447321917f
daridev <darideveloper@gmail.com>
2025-04-22 16:20:48 -0600

    feat (project): redirect / to /admin

--- project/urls.py
+++ project/urls.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    # redirects
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
+
+    # Admin urls
     path('admin/', admin.site.urls),
 ]
 
